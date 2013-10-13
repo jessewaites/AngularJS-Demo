0f5f975e150b01b7639f2e4124bc18a8f60a8736
Jesse Waites <jesse.waites@gmail.com>
2013-10-13 00:15:34 -0500

    worlds shortest angularjs demo app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Angular::Application.config.secret_key_base = 'f17d19a8ab7cb3cc1513b68f2dabae87de9a946b4fc33ace75ea80e16ec46ce511b65e86bc55d70b3e7ae985ad1ce18504a5600df09e8b49dd32dd16631a9f06'
